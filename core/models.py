af812b14c4d9dd01d3d319da7cf771ecbed3efc4
NMR6996 <cefersadiq@gmail.com>
2023-10-21 10:02:11 -0400

    some changes

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class Phones(models.Model):
+class Phone(models.Model):
     brand = models.CharField(max_length=100, blank=True, null=True)
     model = models.CharField(max_length=100, blank=True, null=True)
     imei = models.CharField(max_length=100)
