d627a39e71c3b747f520dfaafff7b1e1abac0343
NMR6996 <cefersadiq@gmail.com>
2023-10-21 09:59:45 -0400

    some changes

--- core/models.py
+++ core/models.py
@@ -1,15 +1,5 @@
-from django.utils import timezone
 from django.db import models
 
-import threading
-import time
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -17,7 +7,7 @@ from django.dispatch import receiver
 class Phones(models.Model):
     brand = models.CharField(max_length=100, blank=True, null=True)
     model = models.CharField(max_length=100, blank=True, null=True)
-    imei = models.IntegerField()
+    imei = models.CharField(max_length=100)
     note = models.TextField(blank=True, null=True)
     added_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=100, default="Qedyiyyatdan keçirilməyib!")
