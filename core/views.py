d106683bed1e3073d666c2693f8dcb3f8397eacc
NMR6996 <cefersadiq@gmail.com>
2023-10-21 09:08:03 -0400

    some changes

--- core/views.py
+++ core/views.py
@@ -1,10 +1,3 @@
-from django.shortcuts import render
-
-from .models import Phones
-
-
 # Create your views here.
-def index(request):
-    phones = Phones.objects.all()
-    return render(request, "index.html", {"phones": phones})
+
 
